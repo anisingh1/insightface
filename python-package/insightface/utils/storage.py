91b8fc09661fdc8453e04c3fe39dedf06a2b761d
Anirudh Singh <anisingh@adobe.com>
2024-12-30 21:23:08 +0530

    Update storage.py

--- python-package/insightface/utils/storage.py
+++ python-package/insightface/utils/storage.py
@@ -6,13 +6,13 @@ from .download import download_file
 
 BASE_REPO_URL = 'https://github.com/deepinsight/insightface/releases/download/v0.7'
 
-def download(sub_dir, name, force=False, root='~/.insightface'):
+def download(name, force=False, root='~/.insightface'):
     _root = os.path.expanduser(root)
-    dir_path = os.path.join(_root, sub_dir, name)
+    dir_path = os.path.join(_root, name)
     if osp.exists(dir_path) and not force:
         return dir_path
     print('download_path:', dir_path)
-    zip_file_path = os.path.join(_root, sub_dir, name + '.zip')
+    zip_file_path = os.path.join(_root, name + '.zip')
     model_url = "%s/%s.zip"%(BASE_REPO_URL, name)
     download_file(model_url,
              path=zip_file_path,
@@ -24,17 +24,16 @@ def download(sub_dir, name, force=False, root='~/.insightface'):
     #os.remove(zip_file_path)
     return dir_path
 
-def ensure_available(sub_dir, name, root='~/.insightface'):
-    return download(sub_dir, name, force=False, root=root)
+def ensure_available(name, root='~/.insightface'):
+    return download(name, force=False, root=root)
 
-def download_onnx(sub_dir, model_file, force=False, root='~/.insightface', download_zip=False):
+def download_onnx(model_file, force=False, root='~/.insightface', download_zip=False):
     _root = os.path.expanduser(root)
-    model_root = osp.join(_root, sub_dir)
-    new_model_file = osp.join(model_root, model_file)
+    new_model_file = osp.join(_root, model_file)
     if osp.exists(new_model_file) and not force:
         return new_model_file
-    if not osp.exists(model_root):
-        os.makedirs(model_root)
+    if not osp.exists(_root):
+        os.makedirs(_root)
     print('download_path:', new_model_file)
     if not download_zip:
         model_url = "%s/%s"%(BASE_REPO_URL, model_file)
@@ -48,5 +47,5 @@ def download_onnx(sub_dir, model_file, force=False, root='~/.insightface', downl
                  path=zip_file_path,
                  overwrite=True)
         with zipfile.ZipFile(zip_file_path) as zf:
-            zf.extractall(model_root)
+            zf.extractall(_root)
         return new_model_file
